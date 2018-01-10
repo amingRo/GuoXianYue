c5d15759be62316946ca12a8f8429dd65d5511ae
guoxianyue <guoxianyue@megvii.com>
2018-01-10 12:59:44 +0800

    支付宝 测试

---
+++ src/components/loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="loading">
+    <div class="loading-circle"></div>
+    <div class="loading-avatar"><img src="images/avatar@2x.jpg" alt="" width="100" height="100"></div>
+    <div class="loading-info">正在努力加载中...</div>
+  </div>
+</template>
+<script>
+
+</script>
+<style scope>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import zfb from '@/components/zfb'
 
 Vue.use(Router)
 
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/zfb',
+      name: 'zfb',
+      component: zfb
     }
   ]
 })
